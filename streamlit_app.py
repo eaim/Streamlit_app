5fe15e948e88ede1f67d5f03058885ab6ee7e9be
Rajkap <34119577+Rajkap@users.noreply.github.com>
2021-09-10 20:49:38 +0630

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -14,6 +14,44 @@ add_selectbox = st.sidebar.selectbox(
 
 st.title('Visualizing world countries data')
 
+#*************************************#
+template_dict = collections.defaultdict(dict)
+template_dirs = [
+    f for f in os.scandir("templates") if f.is_dir() and f.name != "example"
+]
+# TODO: Find a good way to sort templates, e.g. by prepending a number to their name
+#   (e.g. 1_Image classification_PyTorch).
+template_dirs = sorted(template_dirs, key=lambda e: e.name)
+for template_dir in template_dirs:
+    try:
+        # Templates with task + framework.
+        task, framework = template_dir.name.split("_")
+        template_dict[task][framework] = template_dir.path
+    except ValueError:
+        # Templates with task only.
+        template_dict[template_dir.name] = template_dir.path
+# print(template_dict)
+
+
+with st.sidebar:
+    st.info(
+        "🎈 **NEW:** Add your own code template to this site! [Guide](https://github.com/jrieke/traingenerator#adding-new-templates)"
+    )
+    # st.error(
+    #     "Found a bug? [Report it](https://github.com/jrieke/traingenerator/issues) 🐛"
+    # )
+    st.write("## Task")
+    task = st.selectbox(
+        "Which problem do you want to solve?", list(template_dict.keys())
+    )
+    if isinstance(template_dict[task], dict):
+        framework = st.selectbox(
+            "In which framework?", list(template_dict[task].keys())
+        )
+        template_dir = template_dict[task][framework]
+    else:
+        template_dir = template_dict[task]
+#*************************************#
 st.header('Here are some of the countries data in each region')
 
 raw_df = pd.read_csv('https://raw.githubusercontent.com/Rajkap/EDA_on_Would_countries_data/main/world-countries-data-analysis/proj_data/countries%20of%20the%20world.csv')
