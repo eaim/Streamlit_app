542c5d609508904e41892698755f8241872d2656
Rajkap <34119577+Rajkap@users.noreply.github.com>
2021-09-11 11:59:12 +0630

    Update new_copy.py

--- new_copy.py
+++ new_copy.py
@@ -40,68 +40,24 @@ if os.getenv("GITHUB_TOKEN") and os.getenv("REPO_NAME"):
 
 else:
     colab_enabled = False
-
-
-
-
-
-
-# template_dict = collections.defaultdict(dict)
-# template_dirs = [
-#     f for f in os.scandir("templates") if f.is_dir() and f.name != "example"
-# ]
-# # TODO: Find a good way to sort templates, e.g. by prepending a number to their name
-# #   (e.g. 1_Image classification_PyTorch).
-# template_dirs = sorted(template_dirs, key=lambda e: e.name)
-# for template_dir in template_dirs:
-#     try:
-#         # Templates with task + framework.
-#         task, framework = template_dir.name.split("_")
-#         template_dict[task][framework] = template_dir.path
-#     except ValueError:
-#         # Templates with task only.
-#         template_dict[template_dir.name] = template_dir.path
-# # print(template_dict)
-
-
+   
 template_dirs = pd.DataFrame()
-template_dirs['dir'] = [
-    f for f in os.scandir("templates") if f.is_dir() 
-]
-
-template_dirs['dir']= sorted(template_dirs['dir'], key=lambda e: e.name)
-st.write(template_dirs['dir'])
-
-# for f in os.scandir("templates"):
-#     if f.is_dir():
-#         st.write(f)
-#     else:
-#         st.write('nothing')
-        
-     
-
-                      
+template_dirs['dir'] = []
+for f in os.scandir("templates"):
+    for img in f:
+        template_dirs['dir'].append(img)
 
 
 
-with st.sidebar:
-    st.info(
-        "🎈 **NEW:** Add your own code template to this site! [Guide](https://github.com/jrieke/traingenerator#adding-new-templates)"
-    )
-    # st.error(
-    #     "Found a bug? [Report it](https://github.com/jrieke/traingenerator/issues) 🐛"
-    # )
-    st.write("## Task")
-    task = st.selectbox(
-        "Which problem do you want to solve?", template_dirs['dir']
-    )
-    if isinstance(template_dict[task], dict):
-        framework = st.selectbox(
-            "In which framework?", list(template_dict[task].keys())
-        )
-        template_dir = template_dict[task][framework]
-    else:
-        template_dir = template_dict[task]
-# *************************************#
-st.header('Here are some of the countries data in each region')
+img_dirs = pd.DataFrame()
+img_dirs['dir'] = ['Messi','Obama','Jackie_Chan','Daw_Su']
+option = st.sidebar.selectbox(
+    'Select One Person',img_dirs['dir'])
+
+if (option == 'Messi'):
+    st.sidebar.selectbox("Choose any one image", template_dirs['dir']
+
+
+
+
 
